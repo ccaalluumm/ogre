2a02970923c6932784f6bf3e9678f1f751c0786f
Callum <callum.lawsonsmith@gmail.com>
2020-07-30 21:39:47 +1000

    Get VSCode downloaded Utilised the Net.WebClient API to download the VSCode installer binary onto the machine.

--- Program.fs
+++ Program.fs
@@ -1,8 +1,16 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open System
 
 [<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+let main _ =
+    let downloader = new Net.WebClient()
+    //downloader.Headers.Add("User-Agent", "Mozilla/5.0")
+
+    downloader.DownloadFileAsync(Uri("https://aka.ms/win32-x64-user-stable"), "vscode.exe")
+
+    while downloader.IsBusy do
+        printf "Downloading\n"
+    
+    0 // return an exit code
+
+
+    
