3d1aab56d2762d27880c3f1b9e2cb2d97b12edde
Callum <callum.lawsonsmith@gmail.com>
2020-07-31 00:29:15 +1000

    Add spotify download The program now downloads the spotify installer

--- Program.fs
+++ Program.fs
@@ -1,9 +1,8 @@
 ﻿open System
 
-let rec downloading (downloader: Net.WebClient)=
+let rec checkDownloadComplete(downloader: Net.WebClient)=
     if downloader.IsBusy then 
-        downloading downloader
-    else Console.WriteLine("Done!")
+        checkDownloadComplete downloader
 
 [<EntryPoint>]
 let main _ =
@@ -11,8 +10,15 @@ let main _ =
 
     downloader.DownloadFileAsync(Uri("https://aka.ms/win32-x64-user-stable"), "vscode.exe")
 
-    Console.WriteLine("Downloading...")
-    downloading downloader
+    Console.WriteLine("Downloading Visual Studio Code...")
+    checkDownloadComplete downloader
+    
+    
+    downloader.DownloadFileAsync(Uri("https://download.scdn.co/SpotifySetup.exe"), "spotify.exe") 
+    Console.WriteLine("Downloading Spotify...")
+    checkDownloadComplete downloader
+
+    Console.WriteLine("Downloads complete")
     
     0 // return an exit code
 
