c94ec9af81577b956d00aa02615ed04432a84163
Callum <callum.lawsonsmith@gmail.com>
2020-08-03 14:54:02 +1000

    Rewrite project in C# Unfortunately there were difficulties getting F# to work correctly. To bolster ease of programming, especially interfacing with the .NET API, C# is a better choice.
    Included in this commit is a download progress function that prints the
    number of bytes downloaded.

---
+++ Downloader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ogre
+{
+    class Downloader
+    {
+        public void DownloadPrograms(Dictionary<string, string> programs)
+        {
+            WebClient downloader = new WebClient();
+            downloader.DownloadProgressChanged += new DownloadProgressChangedEventHandler(PrintDownloadProgress);
+            
+            foreach(var program in programs)
+            {
+                Console.WriteLine("Downloading " + program.Key);
+                downloader.DownloadFileAsync(new Uri(program.Value), program.Key);
+                
+                while (downloader.IsBusy) { };
+            }
+        }
+
+        private void PrintDownloadProgress(object sender, DownloadProgressChangedEventArgs e)
+        {
+            Console.WriteLine("{0} downloaded {1} of {2} megabytes. {3}% complete...",
+                (string)e.UserState,
+                e.BytesReceived / 1024,
+                e.TotalBytesToReceive / 1024,
+                e.ProgressPercentage);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ogre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var programs = new Dictionary<string, string>
+            {
+                { "vscode.exe", "https://aka.ms/win32-x64-user-stable" },
+                { "docker.exe", "https://download.docker.com/win/stable/Docker%20Desktop%20Installer.exe" },
+                { "chrome.exe", "https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B278D23FF-2DDB-4E40-524D-973D5996950C%7D%26lang%3Den%26browser%3D3%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/chrome/install/ChromeStandaloneSetup64.exe" },
+                { "git.exe", "https://github.com/git-for-windows/git/releases/download/v2.28.0.windows.1/Git-2.28.0-64-bit.exe" },
+                { "spotify.exe", "https://download.scdn.co/SpotifySetup.exe" }
+            };
+
+            Downloader downloader = new Downloader();
+            downloader.DownloadPrograms(programs);
+        }
+    }
+}
