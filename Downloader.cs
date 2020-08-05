52ff4e39053f00460c7618aa38fbe91c005e4d88
Callum <callum.lawsonsmith@gmail.com>
2020-08-05 14:53:03 +1000

    Remove unused imports A bunch of unused .NET libraries were imported by default when creating the project. These have been removed.

--- Downloader.cs
+++ Downloader.cs
@@ -1,13 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Linq;
 using System.Net;
-using System.Runtime.CompilerServices;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace ogre
 {
