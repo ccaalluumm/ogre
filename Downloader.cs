7cc34b1c07b8f20109b393b32eaaec6ab17d0d0b
Callum <callum.lawsonsmith@gmail.com>
2020-08-05 14:18:23 +1000

    Issue #1 Implemented Download Progress Bar Thanks to Honey The Codewitch, a download progress bar was implemented to represent the download progress.

--- Downloader.cs
+++ Downloader.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ogre
@@ -21,18 +22,39 @@ namespace ogre
             {
                 Console.WriteLine("Downloading " + program.Key);
                 downloader.DownloadFileAsync(new Uri(program.Value), program.Key);
-                
+
                 while (downloader.IsBusy) { };
+
+                // Sleep to ensure that the downloader fully finishes, otherwise the new line gets printed prematurely
+                Thread.Sleep(10);
+                Console.Write('\n');
             }
         }
 
         private void PrintDownloadProgress(object sender, DownloadProgressChangedEventArgs e)
         {
-            Console.WriteLine("{0} downloaded {1} of {2} megabytes. {3}% complete...",
-                (string)e.UserState,
-                e.BytesReceived / 1024,
-                e.TotalBytesToReceive / 1024,
-                e.ProgressPercentage);
+            WriteProgressBar(e.ProgressPercentage, true);
+            Thread.Sleep(50);
+        }
+
+        /// <summary>
+        /// Prints a progress bar to the console, using math to print a ■ character for every 10% of the download completed.
+        /// Although refactored to suit this programs needs, full credit to this original design goes to Honey The Codewitch (https://www.codeproject.com/Members/code-witch)
+        /// </summary>
+        /// <param name="percent"></param>
+        /// <param name="update"></param>
+        private static void WriteProgressBar(int percent, bool update = false)
+        {
+            const char block = '■';
+            const char space = ' ';
+            const string back = "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b";
+            string completed = new string(block, (int)Math.Floor((float)percent / 10));
+            string uncompleted = new string(space, 10 - (int)Math.Floor((float)percent / 10));
+
+            if (update)
+            {
+                Console.Write(back + "[" + completed + uncompleted + "]" + " {0,3:##0}%", percent);
+            }
         }
     }
 }
